dc0fc20f5e5fa6ce7cb8206ba17e127ce901b2f9
Stan Runge <stan@stanrunge.dev>
2024-06-30 18:32:34 +0200

    Respond to requests

--- src/endpoints/sendMessages.ts
+++ src/endpoints/sendMessages.ts
@@ -1,1 +1,4 @@
-export async function sendMessages(messages: string[], matchId: number) {}
+export async function sendMessages(messages: string[], matchId: number) {
+  console.log("Sending messages to match with ID: ", matchId);
+  console.log("Messages: ", messages);
+}
