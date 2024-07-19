23dba0f87d850ea9bead852ac2b080685509479c
Stan Runge <stan@stanrunge.dev>
2024-07-19 21:42:10 +0200

    Periodically check for player states

--- src/endpoints/createMatch.ts
+++ src/endpoints/createMatch.ts
@@ -1,39 +1,16 @@
-import { BanchoClient, BanchoMultiplayerChannel } from "bancho.js";
+import {
+  BanchoClient,
+  BanchoLobbyTeams,
+  BanchoMultiplayerChannel,
+} from "bancho.js";
 import { SupabaseClient } from "@supabase/supabase-js";
 import { matchEnded } from "../events/matchEnded.ts";
 import { matchStarted } from "../events/matchStarted.ts";
 import { message } from "../events/message.ts";
-import { playerJoined } from "../events/playerJoined.ts";
-import { playerLeft } from "../events/playerLeft.ts";
 import { playerReady } from "../events/playerReady.ts";
 import type { Score } from "osu-api-extended/dist/types/v2/matches_detaIls";
 
-async function clearMatchQueue(id: number, supabase: SupabaseClient) {
-  const matchQueue = await supabase
-    .from("match_queue")
-    .select("*")
-    .eq("match_id", id)
-    .maybeSingle();
-
-  if (matchQueue.data?.position !== 1) {
-    return;
-  }
-
-  await supabase
-    .from("match_queue")
-    .update({ position: null })
-    .eq("match_id", id);
-}
-
-export async function createMatch(
-  id: number,
-  banchoClient: BanchoClient,
-  supabase: SupabaseClient
-) {
-  console.log("Creating match: ", id);
-
-  await clearMatchQueue(id, supabase);
-
+async function getMatch(supabase: SupabaseClient, id: number) {
   const match = await supabase
     .from("matches")
     .select(
@@ -111,80 +88,102 @@ export async function createMatch(
     throw match.error;
   }
 
-  async function checkMatchParticipants() {
-    for (const matchParticipant of match.data.match_participants) {
-      for (const matchParticipantPlayer of matchParticipant.match_participant_players) {
-        const osuUsername =
-          matchParticipantPlayer.team_members.user_profiles.user_platforms.find(
-            (pf: any) => pf.platforms.name === "osu! (username)"
-          )?.value;
+  return match;
+}
 
-        if (!osuUsername) {
-          console.error(
-            "osu! username not found for participant",
-            matchParticipantPlayer.id
-          );
-          continue;
-        }
+async function clearMatchQueue(id: number, supabase: SupabaseClient) {
+  const matchQueue = await supabase
+    .from("match_queue")
+    .select("*")
+    .eq("match_id", id)
+    .maybeSingle();
 
-        const banchoUser = await banchoClient.getUser(String(osuUsername));
+  if (matchQueue.data?.position !== 1) {
+    return;
+  }
 
-        const { data: matchParticipantPlayerState, error } = await supabase
-          .from("match_participant_players")
-          .select("*")
-          .eq("id", matchParticipantPlayer.id)
-          .single();
+  await supabase
+    .from("match_queue")
+    .update({ position: null })
+    .eq("match_id", id);
+}
 
-        if (error) {
-          console.error("Error fetching participant state:", error);
-          continue;
-        }
+async function checkMatchParticipants(
+  match: any,
+  supabase: SupabaseClient,
+  channel: BanchoMultiplayerChannel
+) {
+  for (const [
+    index,
+    matchParticipant,
+  ] of match.data.match_participants.entries()) {
+    for (const matchParticipantPlayer of matchParticipant.match_participant_players) {
+      const matchParticipantPlayerState = await supabase
+        .from("match_participant_players")
+        .select("state")
+        .eq("id", matchParticipantPlayer.id)
+        .single();
 
-        try {
-          const whois = await banchoUser.whois();
+      const osuId =
+        matchParticipantPlayer.team_members.user_profiles.user_platforms.find(
+          (pf: any) => pf.platforms.name === "osu!"
+        )?.value;
 
-          const isInLobby = whois.channels.some(
-            (channel) => channel.name === match.data.lobby_id
-          );
+      console.log("Checking player: ", osuId);
 
-          if (isInLobby) {
-            if (
-              matchParticipantPlayerState?.state === 1 ||
-              matchParticipantPlayerState?.state === 2
-            ) {
-              await supabase
-                .from("match_participant_players")
-                .update({ state: 3 })
-                .eq("id", matchParticipantPlayer.id);
+      const lobbyPlayer = await channel.lobby.getPlayerById(osuId);
 
-              console.log("Player is in the lobby:", banchoUser.username);
-            }
-          } else {
-            if (matchParticipantPlayerState?.state === 1) {
-              await supabase
-                .from("match_participant_players")
-                .update({ state: 2 })
-                .eq("id", matchParticipantPlayer.id);
-            }
+      await channel.lobby.changeTeam(
+        lobbyPlayer,
+        index % 2 === 0 ? BanchoLobbyTeams.Red : BanchoLobbyTeams.Blue
+      );
+      await channel.lobby.movePlayer(lobbyPlayer, 0);
 
-            console.log("Player is not in the lobby:", banchoUser.username);
-          }
-        } catch (e) {
-          console.error("Error in whois check for user:", osuUsername, e);
+      if (
+        channel.lobby.slots.some((slot) => {
+          return slot?.user.id == osuId;
+        })
+      ) {
+        if (matchParticipantPlayerState.data!.state !== 3) {
           await supabase
             .from("match_participant_players")
-            .update({ state: 1 })
+            .update({ state: 3 })
             .eq("id", matchParticipantPlayer.id);
-        }
 
-        console.log("Checked player:", banchoUser.username);
+          console.log("Player is in the lobby:", osuId);
+        }
+      } else {
+        try {
+          const whois = await lobbyPlayer.user.whois();
+
+          if (matchParticipantPlayerState.data!.state !== 2) {
+            await supabase
+              .from("match_participant_players")
+              .update({ state: 2 })
+              .eq("id", matchParticipantPlayer.id);
+
+            console.log("Player is not in the lobby:", osuId);
+          }
+        } catch (e) {
+          if (matchParticipantPlayerState.data!.state !== 1) {
+            await supabase
+              .from("match_participant_players")
+              .update({ state: 1 })
+              .eq("id", matchParticipantPlayer.id);
+
+            console.log("Player is not in the lobby:", osuId);
+          }
+        }
       }
     }
   }
+}
 
-  // Run the check periodically every minute
-  setInterval(checkMatchParticipants, 5000); // 60000 ms = 1 minute
-
+async function getOrMakeChannel(
+  supabase: SupabaseClient,
+  banchoClient: BanchoClient,
+  match: any
+) {
   let channel: BanchoMultiplayerChannel;
 
   if (match.data.lobby_id) {
@@ -208,13 +207,15 @@ export async function createMatch(
       await supabase
         .from("matches")
         .update({ lobby_id: "#mp_" + channel.lobby.id })
-        .eq("id", id);
+        .eq("id", match.data.id);
 
       await channel.lobby.setSettings(
         2,
         3,
         match.data.match_participants.length
       );
+
+      await channel.lobby.lockSlots();
 
       console.log(
         "Set lobby settings: ",
@@ -249,13 +250,116 @@ export async function createMatch(
     }
   }
 
+  return channel!;
+}
+
+export async function checkScores(
+  channel: BanchoMultiplayerChannel,
+  supabase: SupabaseClient,
+  match: any
+) {
+  const scores = channel.lobby.scores;
+
+  const matchMap = await supabase
+    .from("match_maps")
+    .select("id")
+    .eq("match_id", match.data.id)
+    .order("created_at", { ascending: false })
+    .limit(1)
+    .single();
+
+  if (matchMap.error) {
+    throw matchMap.error;
+  }
+
+  for (const score of scores) {
+    const scores = await supabase
+      .from("scores")
+      .select(
+        `id,
+          match_map_id,
+          match_participant_players!inner(
+            match_participants!inner(
+              match_id, participants(
+                team_id
+              )
+            )
+          ,team_members(
+            user_profiles(
+              user_platforms(value, platform_id))))`
+      )
+      .eq(
+        "match_participant_players.team_members.user_profiles.user_platforms.value",
+        score.player.user.id
+      )
+      .eq(
+        "match_participant_players.team_members.user_profiles.user_platforms.platforms.name",
+        "osu!"
+      )
+      .eq(
+        "match_participant_players.match_participants.match_id",
+        match.data.id
+      )
+      .eq("match_map_id", matchMap.data.id);
+
+    if (scores.error) {
+      throw scores.error;
+    }
+
+    if (scores.data.length > 1) {
+      console.log("Adding extra point to Stan 2.");
+
+      await supabase
+        .from("scores")
+        .update({ score: score.score + 1, failed: !score.pass })
+        .eq("id", scores.data[0].id);
+
+      await supabase
+        .from("scores")
+        .update({ score: score.score, failed: !score.pass })
+        .eq("id", scores.data[1].id);
+    } else {
+      await supabase
+        .from("scores")
+        .update({ score: score.score, failed: !score.pass })
+        .in(
+          "id",
+          scores.data.map((score) => score.id)
+        );
+    }
+  }
+}
+
+async function checkLobbyExists() {
+  return true;
+}
+
+export async function createMatch(
+  id: number,
+  banchoClient: BanchoClient,
+  supabase: SupabaseClient
+) {
+  console.log("Creating match: ", id);
+
+  await clearMatchQueue(id, supabase);
+
+  const match = await getMatch(supabase, id);
+
+  setInterval(() => checkMatchParticipants(match, supabase, channel), 3000);
+
+  let channel = await getOrMakeChannel(supabase, banchoClient, match);
+
+  setInterval(() => checkLobbyExists(), 10000);
+
+  setInterval(() => checkScores(channel, supabase, match), 5000);
+
   channel.lobby.on("matchAborted", () => {
-    matchEnded([], supabase, channel, match);
+    matchEnded(supabase, channel, match);
   });
 
   //@ts-ignore
   channel.lobby.on("matchFinished", (scores: Score[]) => {
-    matchEnded(scores, supabase, channel, match);
+    matchEnded(supabase, channel, match);
   });
 
   channel.lobby.on("playing", () => {
@@ -264,14 +368,6 @@ export async function createMatch(
 
   channel.on("message", async (msg) => {
     message(msg, channel, supabase, match.data.id);
-  });
-
-  channel.lobby.on("playerJoined", async (user) => {
-    playerJoined(user, supabase, match.data.id);
-  });
-
-  channel.lobby.on("playerLeft", async (user) => {
-    playerLeft(user, supabase, match.data.id);
   });
 
   // Player moved
--- src/events/matchEnded.ts
+++ src/events/matchEnded.ts
@@ -1,172 +1,106 @@
 import type { SupabaseClient } from "@supabase/supabase-js";
 import type { BanchoMultiplayerChannel } from "bancho.js";
-import type { Score } from "osu-api-extended/dist/types/v2/matches_detaIls";
 import { changeAllPlayersState } from "../utils/states";
 
-export async function matchEnded(
-  scores: Score[],
+async function updateMatchQueue(supabase: SupabaseClient) {
+  const matchQueue = await supabase
+    .from("match_queue")
+    .select("id, position")
+    .gt("position", 0);
+
+  for (const match of matchQueue.data) {
+    await supabase
+      .from("match_queue")
+      .update({ position: match.position - 1 })
+      .eq("id", match.id);
+  }
+}
+
+async function handleMatchWin(
+  matchParticipantPlayerIndex: number,
   supabase: SupabaseClient,
   channel: BanchoMultiplayerChannel,
   match: any
 ) {
-  async function checkForMatchEnd() {
-    const matchMaps = await supabase
-      .from("match_maps")
-      .select("scores(score, match_participant_players(match_participant_id))")
-      .eq("match_id", match.data.id);
+  await supabase
+    .from("matches")
+    .update({ ongoing: false })
+    .eq("id", match.data.id);
 
-    if (matchMaps.error) {
-      throw matchMaps.error;
-    }
+  const matchParticipantPlayer =
+    match.data.match_participants[matchParticipantPlayerIndex]
+      .match_participant_players[0];
 
-    const pointsTeam1 = matchMaps.data.filter(
-      (match_map) =>
-        match_map.scores
-          .filter(
-            (score) =>
-              score.match_participant_players.match_participant_id ==
-              match.data.match_participants[0].id
-          )
-          .reduce((sum, score) => sum + score.score, 0) >
-        match_map.scores
-          .filter(
-            (score) =>
-              score.match_participant_players.match_participant_id ==
-              match.data.match_participants[1].id
-          )
-          .reduce((sum, score) => sum + score.score, 0)
-    ).length;
+  await channel.sendMessage(
+    `The match has been won by ${matchParticipantPlayer.team_members.user_profiles.name}`
+  );
 
-    const pointsTeam2 = matchMaps.data.filter(
-      (match_map) =>
-        match_map.scores
-          .filter(
-            (score) =>
-              score.match_participant_players.match_participant_id ==
-              match.data.match_participants[1].id
-          )
-          .reduce((sum, score) => sum + score.score, 0) >
-        match_map.scores
-          .filter(
-            (score) =>
-              score.match_participant_players.match_participant_id ==
-              match.data.match_participants[0].id
-          )
-          .reduce((sum, score) => sum + score.score, 0)
-    ).length;
+  await channel.lobby.closeLobby();
 
-    if (pointsTeam1 > match.data.rounds.best_of / 2) {
-      await handleMatchWin(0);
-    }
+  await changeAllPlayersState(1, match.data.id, supabase);
 
-    if (pointsTeam2 > match.data.rounds.best_of / 2) {
-      await handleMatchWin(1);
-    }
+  await updateMatchQueue(supabase);
+}
+
+export async function matchEnded(
+  supabase: SupabaseClient,
+  channel: BanchoMultiplayerChannel,
+  match: any
+) {
+  await changeAllPlayersState(4, match.data.id, supabase);
+
+  const matchMaps = await supabase
+    .from("match_maps")
+    .select("scores(score, match_participant_players(match_participant_id))")
+    .eq("match_id", match.data.id);
+
+  if (matchMaps.error) {
+    throw matchMaps.error;
   }
 
-  async function handleMatchWin(matchParticipantPlayerIndex: number) {
-    await supabase
-      .from("matches")
-      .update({ ongoing: false })
-      .eq("id", match.data.id);
+  const pointsTeam1 = matchMaps.data.filter(
+    (match_map) =>
+      match_map.scores
+        .filter(
+          (score) =>
+            score.match_participant_players.match_participant_id ==
+            match.data.match_participants[0].id
+        )
+        .reduce((sum, score) => sum + score.score, 0) >
+      match_map.scores
+        .filter(
+          (score) =>
+            score.match_participant_players.match_participant_id ==
+            match.data.match_participants[1].id
+        )
+        .reduce((sum, score) => sum + score.score, 0)
+  ).length;
 
-    const matchParticipantPlayer =
-      match.data.match_participants[matchParticipantPlayerIndex]
-        .match_participant_players[0];
+  const pointsTeam2 = matchMaps.data.filter(
+    (match_map) =>
+      match_map.scores
+        .filter(
+          (score) =>
+            score.match_participant_players.match_participant_id ==
+            match.data.match_participants[1].id
+        )
+        .reduce((sum, score) => sum + score.score, 0) >
+      match_map.scores
+        .filter(
+          (score) =>
+            score.match_participant_players.match_participant_id ==
+            match.data.match_participants[0].id
+        )
+        .reduce((sum, score) => sum + score.score, 0)
+  ).length;
 
-    await channel.sendMessage(
-      `The match has been won by ${matchParticipantPlayer.team_members.user_profiles.name}`
-    );
+  if (pointsTeam1 > match.data.rounds.best_of / 2) {
+    await handleMatchWin(0, supabase, channel, match);
+  }
 
-    await channel.lobby.closeLobby();
-
-    await changeAllPlayersState(1, match.data.id, supabase);
-
-    const matchQueue = await supabase
-      .from("match_queue")
-      .select("id, position")
-      .gt("position", 0);
-
-    for (const match of matchQueue.data) {
-      await supabase
-        .from("match_queue")
-        .update({ position: match.position - 1 })
-        .eq("id", match.id);
-    }
+  if (pointsTeam2 > match.data.rounds.best_of / 2) {
+    await handleMatchWin(1, supabase, channel, match);
   }
 
   console.log("Match finished");
-
-  for (const score of scores) {
-    await changeAllPlayersState(4, match.data.id, supabase);
-
-    const matchMap = await supabase
-      .from("match_maps")
-      .select("id")
-      .eq("match_id", match.data.id)
-      .order("created_at", { ascending: false })
-      .limit(1)
-      .single();
-
-    if (matchMap.error) {
-      throw matchMap.error;
-    }
-
-    const scores = await supabase
-      .from("scores")
-      .select(
-        `id,
-          match_map_id,
-          match_participant_players!inner(
-            match_participants!inner(
-              match_id, participants(
-                team_id
-              )
-            )
-          ,team_members(
-            user_profiles(
-              user_platforms(value, platform_id))))`
-      )
-      .eq(
-        "match_participant_players.team_members.user_profiles.user_platforms.value",
-        score.user_id
-      )
-      .eq(
-        "match_participant_players.team_members.user_profiles.user_platforms.platforms.name",
-        'osu!'
-      )
-      .eq(
-        "match_participant_players.match_participants.match_id",
-        match.data.id
-      )
-      .eq("match_map_id", matchMap.data.id);
-
-    if (scores.error) {
-      throw scores.error;
-    }
-
-    if (scores.data.length > 1) {
-      console.log("Adding extra point to Stan 2.");
-
-      await supabase
-        .from("scores")
-        .update({ score: score.score + 1, failed: !score.passed })
-        .eq("id", scores.data[0].id);
-
-      await supabase
-        .from("scores")
-        .update({ score: score.score, failed: !score.passed })
-        .eq("id", scores.data[1].id);
-    } else {
-      await supabase
-        .from("scores")
-        .update({ score: score.score, failed: !score.passed })
-        .in(
-          "id",
-          scores.data.map((score) => score.id)
-        );
-    }
-
-    await checkForMatchEnd();
-  }
 }
