ff314d7cea5636edf54a4a0315e4af9835d1d9dc
Stan Runge <stan@stanrunge.dev>
2024-07-21 18:47:16 +0200

    Check for match is waiting before starting map Fixes #37

--- src/events/playerReady.ts
+++ src/events/playerReady.ts
@@ -1,7 +1,32 @@
+import type { SupabaseClient } from "@supabase/supabase-js";
 import type { BanchoMultiplayerChannel } from "bancho.js";
 
-export async function playerReady(channel: BanchoMultiplayerChannel) {
+export async function playerReady(
+  channel: BanchoMultiplayerChannel,
+  supabase: SupabaseClient,
+  match: any
+) {
   console.log("All players are ready");
+
+  const matchMaps = await supabase
+    .from("match_maps")
+    .select("status")
+    .eq("match_id", match.data.id)
+    .order("created_at", { ascending: false });
+
+  if (matchMaps.error) {
+    throw matchMaps.error;
+  }
+
+  if (matchMaps.data.length === 0) {
+    console.log("No match maps found for match:", channel.lobby.id);
+    return;
+  }
+
+  if (matchMaps.data[0].status != "waiting") {
+    console.log("Match map has already started.");
+    return;
+  }
 
   channel.lobby.startMatch();
 }
